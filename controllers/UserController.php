49634e7f1a60f6c1ab419af7448f48c3e29341ea
Le Quoc Anh <a003021@pc-003021-1.local>
2023-11-09 01:43:27 +0700

    remove redundant file

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,16 +4,13 @@ namespace app\controllers;
 
 use app\core\Request;
 use app\core\Response;
-use app\dto\UserDto;
 use app\dto\UserLoginDto;
-use app\models\LoginModel;
 use app\models\UserEntity;
 use app\service\JwtService;
 use app\service\UserService;
 use Exception;
 use Ramsey\Uuid\Uuid;
 
-// layer architecture: controller --> service --> repository --> DB
 
 class UserController
 {
@@ -24,55 +21,6 @@ class UserController
         $this->userService = $userService;
     }
 
-
-    // Global error handler
-//    public function register(Request $request, Response $response): void
-//    {
-//        try {
-//           // $userDto = new UserDto($request->id, $request->mail, ....);
-//            $this->userService->register($request->getBody());
-//            $response->render(200, self::SUCCESS, 'Register successfully');
-//        } catch ($error) {
-//            $response->render(400, self::SUCCESS, 'Error');
-//        }
-//
-//
-//
-//        $registerModel = new RegisterModel();
-//        $registerModel->loadData($request->getBody());
-//        if ($registerModel->validate() && $registerModel->save()) {
-//            $response->render(200, self::SUCCESS, 'Register successfully');
-//        } else {
-//            $response->render(404, self::FAILED, 'Register failed');
-//        }
-//    }
-//
-//    // UserService
-//    public function registerService() { // S trong SOLID
-//        // check user exist
-//        $user = $this->userRepositoryInterface->getByEmail($userDto->getMail());
-//        if ($user) {
-//            throw new UserExistException('This user is exist in DB, please try again');
-//        }
-//
-//        // save database
-//        $userEntity = new UserEntity($userDto->getMail(), $userDto->getName());
-//        $this->userRepositoryInterface->save($userEntity);
-//    }
-//
-//    // UserRepository
-//    public function save(UserEntity $user)
-//    {
-//        $query = Insert into.....
-//        // save
-//    }
-//
-//    public function getByEmail(String $email)
-//    {
-//        // Handle SQL injection
-//        // Select * from table_user where email = $email
-//        // save
-//    }
 
     public function register(Request $request, Response $response): void
     {
--- helper/Helper.php
+++ helper/Helper.php
@@ -4,14 +4,5 @@ namespace app\helper;
 
 class Helper
 {
-    public function getAttributesFromObject($object): array
-    {
-        $attributes = [];
 
-        foreach ($object as $key => $value) {
-            $attributes[] = $key;
-        }
-
-        return $attributes;
-    }
 }
